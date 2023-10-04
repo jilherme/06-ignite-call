ffbe3991d6298f4c878434023f728f977445018b
jilherme <rosa.guilhermediogo@gmail.com>
2023-10-04 18:23:07 -0300

    fix: conect calendar typo

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -24,7 +24,7 @@ export default function Register() {
 
       <ConnectBox>
         <ConnectItem>
-          <Text>Google Calendaer</Text>
+          <Text>Google Calendar</Text>
           <Button
             variant="secondary"
             size="sm"
