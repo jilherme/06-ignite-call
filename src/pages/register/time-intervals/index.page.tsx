940ac059ac3bc66efdb1e4b48a232ce2fcbc144f
jilherme <rosa.guilhermediogo@gmail.com>
2023-10-07 18:16:35 -0300

    add(Availability): Zod form validation

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,3 +1,4 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Button,
   Checkbox,
@@ -13,15 +14,33 @@ import { z } from "zod";
 import { getWeekDays } from "@/utils/get-week-days";
 
 import {
+  FormError,
   IntervalBox,
   IntervalDay,
   IntervalInputs,
   IntervalItem,
-  IntervalsContainer,
+  IntervalContainer,
 } from "./styles";
 import { Container, Header } from "../styles";
 
-const timeIntervalsFormSchema = z.object({});
+const timeIntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+      })
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
+    .refine((intervals) => intervals.length > 0, {
+      message: "Você precisa selecionar pelo menos um dia da semana",
+    }),
+});
+
+type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>;
 
 export default function TimeIntervals() {
   const {
@@ -31,15 +50,16 @@ export default function TimeIntervals() {
     handleSubmit,
     formState: { isSubmitting, errors },
   } = useForm({
+    resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
-        { weekday: 0, enabled: false, startTime: "08:00", endTime: "18:00" },
-        { weekday: 1, enabled: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 2, enabled: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 3, enabled: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 4, enabled: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 5, enabled: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 6, enabled: false, startTime: "08:00", endTime: "18:00" },
+        { weekDay: 0, enabled: false, startTime: "08:00", endTime: "18:00" },
+        { weekDay: 1, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekDay: 2, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekDay: 3, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekDay: 4, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekDay: 5, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekDay: 6, enabled: false, startTime: "08:00", endTime: "18:00" },
       ],
     },
   });
@@ -54,7 +74,9 @@ export default function TimeIntervals() {
 
   const intervals = watch("intervals");
 
-  async function handleSetTimeIntervals() {}
+  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
+    console.log(data);
+  }
 
   return (
     <Container>
@@ -69,7 +91,7 @@ export default function TimeIntervals() {
       </Header>
 
       <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
-        <IntervalsContainer>
+        <IntervalContainer>
           {fields.map((field, index) => {
             const isFieldDisabled = intervals[index].enabled === false;
 
@@ -91,7 +113,7 @@ export default function TimeIntervals() {
                       );
                     }}
                   />
-                  <Text>{weekDays[field.weekday]}</Text>
+                  <Text>{weekDays[field.weekDay]}</Text>
                 </IntervalDay>
 
                 <IntervalInputs>
@@ -115,9 +137,17 @@ export default function TimeIntervals() {
               </IntervalItem>
             );
           })}
-        </IntervalsContainer>
+        </IntervalContainer>
 
-        <Button type="submit">
+        {errors.intervals && (
+          <FormError size="sm">{errors.intervals.message}</FormError>
+        )}
+
+        <Button
+          type="submit"
+          aria-disabled={isSubmitting}
+          disabled={isSubmitting}
+        >
           Próximo passo <ArrowRight />
         </Button>
       </IntervalBox>
