97711e44c5f9fc689c82543c559e5daabacc52eb
jilherme <rosa.guilhermediogo@gmail.com>
2023-10-18 23:00:30 -0300

    add(Calendar): prepared props

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Container,
   TimePicker,
@@ -8,11 +10,13 @@ import {
 import { Calendar } from "../../../../../components/Calendar";
 
 export function CalendarStep() {
-  const isDateSelected = true;
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
+
+  const isDateSelected = !!selectedDate;
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar />
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 
       {isDateSelected && (
         <TimePicker>
