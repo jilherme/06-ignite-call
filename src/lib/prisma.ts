0c1d9cbc98293d8dc37ec902b1d79df048e075f7
jilherme <rosa.guilhermediogo@gmail.com>
2023-10-28 12:51:45 -0300

    test with planetscale serverless driver

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { connect } from "@planetscale/database";
+import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
+import { PrismaClient } from "@prisma/client/edge";
+import { fetch as undiciFetch } from "undici";
 
+const connectionString = process.env.DATABASE_URL;
+
+const connection = connect({ url: connectionString, fetch: undiciFetch });
+const adapter = new PrismaPlanetScale(connection);
+const prismaEdge = new PrismaClient({ adapter });
+
+// development
 const prismaClientSingleton = () => {
   // prisma reads from the .env file
 
@@ -14,7 +24,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prisma = globalForPrisma.prisma ?? prismaEdge;
 
 export default prisma;
 
